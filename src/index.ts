64c9f031bcbd5d75c6a690c26c019bd21e783397
takebon <ytakeshi_7@yahoo.co.jp>
2025-02-08 08:58:23 +0900

    first commit

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+import { Hono } from "hono";
+
+const app = new Hono();
+
+app.get("/", (c) => {
+  return c.text("Hello Hono!");
+});
+app.get("/test", (c) => {
+  return c.json({ test: "成功" });
+});
+
+export default app;
